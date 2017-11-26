30e26be91e12fea170ea58cce83dea3764fb7c14
Adam Charnock <adam@adamcharnock.com>
2017-11-26 18:21:31 +0000

    Initial commit

---
+++ README.rst
@@ -0,0 +1,7 @@
+Lightbus Examples
+=================
+
+Here you can find a number of examples in how to use `Lightbus`_.
+
+
+.. _Lightbus: http://lightbus.org
---
+++ setup.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup, find_packages
+
+setup(
+    name='lightbus-examples',
+    version=open('VERSION').read().strip(),
+    author='Adam Charnock',
+    author_email='adam@adamcharnock.com',
+    packages=find_packages(),
+    url='https://github.com/adamcharnock/lightbus-examples',
+    license='MIT',
+    description='Example projects using Lightbus',
+    long_description=open('README.rst').read() if exists("README.rst") else "",
+    install_requires=[
+        'lightbus',
+    ],
+    include_package_data=True,
+)
