9bc05df6904cba4b0c3f0a59c88552c3e3e5a5f6
Adam Charnock <adam@adamcharnock.com>
2017-11-27 20:07:41 +0000

    Example 01 now working following refactor

--- lightbus_examples/ex01_simple/stock/bus.py
+++ lightbus_examples/ex01_simple/stock/bus.py
@@ -1,15 +1,22 @@
-from lightbus_examples.ex01_simple.stock.web import db
+import lightbus
+
+from . import db
 
 
+bus = lightbus.create()
 
-async def handle_change(bus, **kwargs):
+
+async def handle_change(**kwargs):
     products = await bus.products.all.call_async()
     db['products'].delete()
     db['products'].insert_many(products)
 
 
-
 def before_server_start(bus):
+    """
+    before_server_start() within the root bus.py file
+    is called before the server begins serving requests
+    """
     bus.products.created.listen(handle_change)
     bus.products.updated.listen(handle_change)
     bus.products.deleted.listen(handle_change)
